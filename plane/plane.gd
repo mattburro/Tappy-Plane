d557506eb0be5535b97c8f70b06a5dfe153c5d00
Matt Burroughs <matt.burro@gmail.com>
2023-12-24 22:17:40 -0500

    Added laser pipe and pipes scenes

--- plane/plane.gd
+++ plane/plane.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite = $AnimatedSprite2D
 
 const GRAVITY: float = 1800.0
 const POWER: float = -400.0
@@ -11,8 +12,15 @@ func _physics_process(delta):
 	fly()
 	
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = POWER
 		animation_player.play("fly")
+
+func die() -> void:
+	animated_sprite.stop()
+	set_physics_process(false)
